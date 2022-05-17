85fe594841b938fa313b0e13fc78c004a683d0b6
latifa amahri <latifaamahri20@gmail.com>
2022-05-17 12:40:47 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from blog.models import Comment, Post
+from blog.models import Comment, Post 
 
 # Creating views
 def index(request):
     comments = Comment.objects.filter()
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts,"comments":comments})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
