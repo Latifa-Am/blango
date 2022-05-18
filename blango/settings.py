77f451edbad5907528c9d3238eec10e4c65fa98e
latifa amahri <latifaamahri20@gmail.com>
2022-05-18 17:11:46 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -86,10 +86,12 @@ class Dev(Configuration):
         "django.contrib.staticfiles",
         "blog",
         "crispy_forms",
-        "crispy_bootstrap5"
+        "crispy_bootstrap5",
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -118,7 +120,7 @@ class Dev(Configuration):
     ]
 
     WSGI_APPLICATION = 'blango.wsgi.application'
-
+    INTERNAL_IPS = ["192.168.11.179"]
 
     # Database
     # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
