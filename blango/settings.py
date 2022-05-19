af376ac0facaf967426cf019c0a5da0aa036eea1
latifa amahri <latifaamahri20@gmail.com>
2022-05-19 12:43:22 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -186,6 +186,9 @@ class Dev(Configuration):
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     RISPY_TEMPLATE_PACK = "bootstrap5"
     AUTH_USER_MODEL = "blango_auth.User"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+    REGISTRATION_OPEN = True
 
 class Prod(Dev):
     DEBUG = False
